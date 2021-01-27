46f988ee5d830f0c83574d9ef00926883b0c2fd4
Igor Matias <igorhmatias@gmail.com>
2021-01-27 01:00:04 -0300

    feat: config tab bottom navigation

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './src/pages/Home'
 import About from './src/pages/About'
+import {FontAwesome } from '@expo/vector-icons'
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const icons = {
+  Home: {
+    name:'home'
+  },
+  About: {
+    name: 'list'
+  }
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="About" component={About} />
-      </Stack.Navigator>
+      <Tab.Navigator
+      screenOptions={({route})=> ({
+        tabBarIcon:({color, size})=>{
+          const {name} = icons[route.name];
+          return <FontAwesome name={name} color={color} size={size}/>
+
+        }
+      })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="About" component={About} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
