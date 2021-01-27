da42d67908bdd7d4b3a5f38f83ecba007ba181a6
Igor Matias <igorhmatias@gmail.com>
2021-01-27 00:29:06 -0300

    feat: create page about

---
+++ src/pages/About/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View,Text,Button,StyleSheet } from 'react-native';
+
+// import { Container } from './styles';
+
+const About = ({navigation}) => {
+  return (
+      <>
+      <View style={styles.container}>
+          <Text style={styles.author}>Desenvolvido por Igor Matias</Text>
+          <Text style={styles.description}>Curso sujeito programador</Text>
+          
+      </View>
+      <Button
+      onPress={()=> navigation.goBack()} 
+      title="Voltar">
+         
+      </Button>
+      </>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+    },
+    author: {
+        fontSize: 24,
+        fontWeight:'bold',
+        color: '#111',
+    },
+    description: {
+        fontSize: 18,
+    }
+})
+export default About;
